21a1011c972a273114f8bb2e87e484d7076298f1
unknown <ido.bandd@gmail.com>
2023-04-27 14:36:41 +0300

    adjusting router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { router } from './routes'
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-<RouterProvider router={router}>
-            <App />
-          </RouterProvider>
+<RouterProvider router={router} />
 )
